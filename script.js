29a218f7650c864cfafb8a344894b79ef7f17be8
tanvir120765@gmail.com <tanvir120765@gmail.com>
2025-08-27 11:44:05 +0600

    complete hearts incon count

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("file connected")
+
+let count = 0;
+
+// সবগুলো heart element নেবো
+const allHearts = document.getElementsByClassName("hearts");
+
+// প্রতিটা heart এ আলাদা করে event listener বসাবো
+for (const heart of allHearts) {
+  heart.addEventListener("click", function () {
+    count++;
+    document.getElementById("heart-count").innerText = count;
+  });
+}
